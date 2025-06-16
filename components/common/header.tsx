a0bfe8bcc7e4052da3ffb20aee9030252266b6ab
Hemant <guptahemant350@gmail.com>
2025-06-16 22:30:26 +0530

    Header and nav-link completed

--- components/common/header.tsx
+++ components/common/header.tsx
@@ -1,36 +1,40 @@
+'use client';
+
 import React from "react";
 import NavLink from "@/components/common/nav-link";
 import { FileText } from "lucide-react";
-import { Button } from "@/components/ui/button";
 
 const Header = () => {
   const isLoggedIn = false;
 
   return (
-    <nav className="container flex items-center justify-between py-2 lg:px-8 px-2 mx-auto">
+    <nav className="container grid grid-cols-3 items-center py-2 lg:px-8 px-2 mx-auto">
+      {/* Left - Logo */}
       <div className="flex items-center space-x-4">
         <NavLink href="/" className="flex items-center gap-1 lg:gap-2 shrink-0">
-          <FileText className="w-5 h-5 lg:w-8 lg:h-8 text-gray-900 hover:rotate-12 transform tranition duration-200 ease-in-out" />
-          <span className="font-bold lg:text-xl  text-gray-900">Summarion</span>
+          <span className="flex items-center space-x-1">
+            <FileText className="w-5 h-5 lg:w-8 lg:h-8 relative top-[1px] text-gray-900 hover:rotate-12 transform transition duration-200 ease-in-out" />
+            <span className="font-bold lg:text-xl text-gray-900">Summarion</span>
+          </span>
         </NavLink>
       </div>
 
-      <div className="flex gap-4 lg:justify-center lg:items-center lg:gap-12 ">
-        <NavLink href="/#pricing">Pricing</NavLink>
+      {/* Center - Pricing */}
+      <div className="flex justify-center">
+        <NavLink href="/pricing">Pricing</NavLink>
         {isLoggedIn && <NavLink href="/dashboard">Your Summaries</NavLink>}
       </div>
 
-      <div className="flex lg:justify-end lg:flex-1">
+      {/* Right - Auth Buttons */}
+      <div className="flex justify-end gap-4">
         {isLoggedIn ? (
           <div className="flex gap-2 items-center">
             <NavLink href="/upload">Upload a PDF</NavLink>
             <div>Pro</div>
-            <Button>User</Button>
+            {/* <Button>User</Button> */}
           </div>
         ) : (
-          <div>
-            <NavLink href="/#sign-in">Sign In</NavLink>
-          </div>
+          <NavLink href="/sign-in">Sign In</NavLink>
         )}
       </div>
     </nav>
--- components/common/nav-link.tsx
+++ components/common/nav-link.tsx
@@ -1,17 +1,27 @@
+'use client';
+
 import React from "react";
 import Link from "next/link";
-
+import { cn } from "@/lib/utils";
+import { usePathname } from "next/navigation";
 interface NavLinkProps {
   href: string;
   children: React.ReactNode;
   className?: string;
 }
 
-const NavLink = ({ href, children, className}: NavLinkProps) => {
+const NavLink = ({ href, children, className }: NavLinkProps) => {
+  const pathName = usePathname();
+  const isActive = pathName === href || pathName.startsWith(href);
+
   return (
     <Link
       href={href}
-      className="transition-colors text-sm duration-200 text-gray-600 hover:text-rose-500"
+      className={cn(
+        "transition-colors text-sm duration-200 text-gray-600 hover:text-blue-500",
+        isActive && "text-blue-500 font-semibold",
+        className
+      )}
     >
       {children}
     </Link>
