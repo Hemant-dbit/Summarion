c8fe2b8392cb4b8f0016139fde990e72c619adfa
Hemant <guptahemant350@gmail.com>
2025-07-01 23:02:34 +0530

    HomePage:Completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,18 @@
-import Image from "next/image";
-import { Button } from "@/components/ui/button";
+import HeroSection from "@/components/home/hero-section";
+import DemoSection from "@/components/home/demo-section";
+import HowItWorksSection from "@/components/home/how-it-works-section";
+import PricingSection from "@/components/home/pricing-section";
+import CTASection from "@/components/home/cta-section";
 
 export default function Home() {
   return (
     <>
-      <div>
-       
+      <div className="min-h-screen flex flex-col">
+       <HeroSection />
+       <DemoSection /> 
+       <HowItWorksSection />
+       <PricingSection />
+       <CTASection />
       </div>
     </>
   );
